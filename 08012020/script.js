72f43cde067b9f144fb388d6c05af1013c079c5e
Ramon Zamorano <razamorano@gmail.com>
2020-08-01 11:44:47 +1200

    hi

--- 08012020/script.js
+++ 08012020/script.js
@@ -4,20 +4,18 @@ var color2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
 
 
-color1.addEventListener("input", function(){
+function setGradient() {
 	body.style.background = 
 	"linear-gradient(to right, " 
 	+ color1.value 
 	+ ", " 
 	+ color2.value 
-	+ ");";
-});
+	+ ")";
 
-color2.addEventListener("input", function(){
-	body.style.background = 
-	"linear-gradient(to right, " 
-	+ color1.value 
-	+ ", " 
-	+ color2.value 
-	+ ");";
-});
+	css.textContent = body.style.background + ";";
+}
+
+// Called on the html
+// color1.addEventListener("input", setGradient);
+
+// color2.addEventListener("input", setGradient);
